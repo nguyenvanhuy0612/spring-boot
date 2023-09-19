7fa9a83b121abcc2e8c4a616456d41f0b8b222bb
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2023-09-19 22:07:24 +0700

    Update

--- src/main/java/org/huy/demo/topic/TopicController.java
+++ src/main/java/org/huy/demo/topic/TopicController.java
@@ -1,10 +1,9 @@
 package org.huy.demo.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,5 +21,10 @@ public class TopicController {
     @RequestMapping("/topics/{id}")
     public Topic getTopic(@PathVariable String id) {
         return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
     }
 }
--- src/main/java/org/huy/demo/topic/TopicService.java
+++ src/main/java/org/huy/demo/topic/TopicService.java
@@ -2,17 +2,16 @@ package org.huy.demo.topic;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(
+    private final List<Topic> topics = new ArrayList<>(List.of(
             new Topic("spring", "Spring Framework", "Learn Spring"),
             new Topic("java", "Java Core", "Learn Java"),
             new Topic("javascript", "JavaScript", "Learn Javascript")
-    );
-
+    ));
 
     public List<Topic> getAllTopics() {
         return topics;
@@ -24,5 +23,9 @@ public class TopicService {
                 .filter(topic -> topic.getId().equals(id))
                 .findAny()
                 .orElseGet(() -> new Topic("Not found", null, null));
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 }
